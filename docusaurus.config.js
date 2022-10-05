6c285e6df99ae41aa90a1f6b048ebb6a9382947e
Pawel Sawicki <public@psawicki.de>
2022-10-05 12:16:19 +0200

    chaning the deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ufocourier', // Usually your GitHub org/user name.
   projectName: 'ufocourier-website-pre', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
