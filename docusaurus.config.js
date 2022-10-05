b8548f7fe432852e2f47b9ac07e6faf00582dbba
Pawel Sawicki <public@psawicki.de>
2022-10-05 11:49:31 +0200

    Adding trailingSlash option to configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'ufocourier', // Usually your GitHub org/user name.
   projectName: 'ufocourier-website-pre', // Usually your repo name.
   deploymentBranch: 'main',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
