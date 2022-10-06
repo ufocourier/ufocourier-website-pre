2f552b35f8bc6d88e0ab78d35fa84bd5ff34cba7
Pawel Sawicki <public@psawicki.de>
2022-10-06 23:51:50 +0200

    MailChimp, attempt 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,8 @@ const config = {
   scripts: [
     {
       id: 'mcjs',
-      src: 'https://chimpstatic.com/mcjs-connected/js/users/68dd166f70b27de4d248f4e38/ced60a8cad1b7099183b5b1d7.js'
+      src: 'https://chimpstatic.com/mcjs-connected/js/users/68dd166f70b27de4d248f4e38/ced60a8cad1b7099183b5b1d7.js',
+      defer: true
     }
   ],
 
